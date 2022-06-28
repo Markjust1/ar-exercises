811b90e02e46562f14940a50d95562987efc49ef
Markjust1 <novikov711@gmail.com>
2022-06-27 20:26:46 -0600

    ex1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,25 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+s = Store.new
+s.name = "Burnaby"
+s.annual_revenue = 300000
+s.mens_apparel = true
+s.womens_apparel = true
+s.save
+
+s = Store.new
+s.name = "Richmond"
+s.annual_revenue = 1260000
+s.mens_apparel = false
+s.womens_apparel = true
+s.save
+
+s = Store.new
+s.name = "Gastown"
+s.annual_revenue = 190000
+s.mens_apparel = true
+s.womens_apparel = false
+s.save
+
+puts Store.new
